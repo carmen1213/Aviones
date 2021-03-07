98a5e0f18b656c9d1d540f6c01f425da1d9f2bf7
carmen1213 <74176423+carmen1213@users.noreply.github.com>
2021-03-07 19:58:53 +0100

    Add files via upload

---
+++ Avionessininterfaz/src/Aviones.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Aviones {
+
+
+    public static void main(String[] args) {
+        boolean salir = true;
+        Scanner sc = new Scanner(System.in);
+
+        while (salir == true) {
+            System.out.println("\n DATOS DE LOS VUELOS");
+            System.out.println("1-Crear Reserva");
+            System.out.println("2-Cancelar Reserva ");
+            System.out.println("3-Salir");
+            System.out.println("\n Introduce el numero que desee:");
+            int a = sc.nextInt();
+        }
+
+    
+    }
+}
+
